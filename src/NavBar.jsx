f6285371578de602b0dcd6ad4d6ec37be1eaccbf
Nicholas Lee <nicholasleexyz@gmail.com>
2023-09-25 08:37:22 -0600

    daisy ui

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,19 +2,14 @@ import { Link } from "react-router-dom";
 
 export function NavBar() {
   return (
-    <nav className="w-100 h-fit bg-slate-700 flex justify-around text-gray-200">
-      <Link
-        className="w-80 border-t-0 border-b-0 border-l-2 border-r-2 hover:border-dotted border-sky-500 flex-1 text-center"
-        to="/"
-      >
+    <nav className="">
+      <Link className="btn btn-primary" to="/">
         Home
       </Link>
-      <Link
-        className="border-t-0 border-b-0 border-l-2 border-r-2 hover:border-dotted border-sky-500 flex-1 text-center"
-        to="/about"
-      >
+      <Link className="btn btn-secondary" to="/about">
         About
       </Link>
+      <button className="btn">asdf</button>
     </nav>
   );
 }
