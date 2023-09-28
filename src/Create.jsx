ec0dfd55f804ce80c3ede29d95a80d4841405d10
Nicholas Lee <nicholasleexyz@gmail.com>
2023-09-27 23:58:02 -0600

    puzzle editing progress

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,7 +1,22 @@
 export function Create() {
   return (
     <>
-      <h1>Create</h1>
+      {/* list all of the currently available puzzles */}
+      {/* be able to select one of the available puzzles */}
+      {/* load in the currently selected puzzle to a board */}
+      {/* have a board to represent the currently selected puzzle */}
+
+      {/* board */}
+      <div className="flex h-screen w-100 items-center">
+        <div className="grid grid-cols-9 min-w-max w-fit h-fit  bg-red-500">
+          {[...Array(81).keys()].map((i) => (
+            <div
+              key={i}
+              className="w-10 h-10 sm:w-12 sm:h-12 md:w-16 md:h-16 bg-blue-500 border-green-900  border"
+            ></div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
