b4633d77f91de17ea3397327b4a08dd06747932c
Nicholas Lee <nicholasleexyz@gmail.com>
2023-10-04 07:32:12 -0600

    fix

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,13 +1,16 @@
 import PuzzleList from "../components/PuzzleList/PuzzleList";
 import SudokuGridGame from "../components/SudokuGrid/SudokuGridGame";
+import SolvedContextProvider from "../components/contexts/SolvedContextProvider";
 
 export default function Game() {
   return (
     <>
-      <div className="grid sm:grid-cols-4 w-full h-full sm:gap-4 grid-cols-1 justify-center">
-        <SudokuGridGame />
-        <PuzzleList></PuzzleList>
-      </div>
+      <SolvedContextProvider>
+        <div className="grid sm:grid-cols-4 w-full h-full sm:gap-4 grid-cols-1 justify-center">
+          <SudokuGridGame />
+          <PuzzleList></PuzzleList>
+        </div>
+      </SolvedContextProvider>
     </>
   );
 }
