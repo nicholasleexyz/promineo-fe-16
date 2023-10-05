f6e781561d8201596b2acf6b5367f2261d5d6882
Nicholas Lee <nicholasleexyz@gmail.com>
2023-10-05 06:00:53 -0600

    full board solve check

--- src/components/SudokuGrid/SudokuGridGame.jsx
+++ src/components/SudokuGrid/SudokuGridGame.jsx
@@ -25,16 +25,66 @@ export default function SudokuGridGame() {
         .map((pair) => pair[0])
         .flat();
     };
-    setSolved(getSolvedRows());
 
-    //set solved based on flat index
+    const getSolvedColumns = () => {
+      const columns = [...Array(9).keys()].map((j) =>
+        [...Array(9).keys()].map((i) => i * 9 + j)
+      );
+      // console.log(columns);
+      const columnCheck = (column) =>
+        [...new Set(column.map((i) => board[i]).filter((c) => c !== 0))]
+          .length == 9;
 
-    // console.log(rows);
-    // const columns = [...Array(9).keys()].map((i) => board);
+      const s = columns.map(columnCheck);
+      const zip = columns.map((column, i) => [column, s[i]]);
 
-    // console.log(s);
-    // setSolved(s);
-  }, [board]);
+      return zip
+        .filter((pair) => pair[1])
+        .map((pair) => pair[0])
+        .flat();
+    };
+
+    const getSolvedBlocks = () => {
+      // Just for funzies and to make life more interesting if it breaks :P
+      const indexConversion = [...Array(81).keys()].map((i) => {
+        const row = Math.floor(i / 9);
+        const rowOffset = row * 3;
+
+        return (
+          ((Math.floor(i / 3) * 9) % 27) +
+          rowOffset +
+          Math.floor(row / 3) * 18 +
+          (((i % 3) + rowOffset) % 3)
+        );
+      });
+
+      const blocks = [...Array(9).keys()].map((j) =>
+        [...Array(9).keys()].map((i) => indexConversion[j * 9 + i])
+      );
+
+      const blockCheck = (block) =>
+        [...new Set(block.map((i) => board[i]).filter((c) => c !== 0))]
+          .length == 9;
+
+      const s = blocks.map(blockCheck);
+      const zip = blocks.map((block, i) => [block, s[i]]);
+
+      return zip
+        .filter((pair) => pair[1])
+        .map((pair) => pair[0])
+        .flat();
+    };
+
+    const _solved = getSolvedColumns()
+      .concat(getSolvedRows())
+      .concat(getSolvedBlocks());
+
+    setSolved(_solved);
+
+    if (_solved.length == 243) {
+      console.log("You solved the sudoku!");
+    }
+  }, [board, setSolved]);
 
   return (
     <>
