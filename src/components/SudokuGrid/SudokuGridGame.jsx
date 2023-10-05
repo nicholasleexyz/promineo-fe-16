84eb57f8ef6dec12713f27345f7aea22c7d3363a
Nicholas Lee <nicholasleexyz@gmail.com>
2023-10-04 19:52:44 -0600

    rows validation

--- src/components/SudokuGrid/SudokuGridGame.jsx
+++ src/components/SudokuGrid/SudokuGridGame.jsx
@@ -7,9 +7,25 @@ export default function SudokuGridGame() {
   const { board, setBoard } = useBoardContext();
 
   useEffect(() => {
-    // const columns = [...Array(9).keys()].map(i => board)
-    // setSolved([...Array(81).keys()].filter((i) => !!(i % 9)));
-  }, []);
+    const rows = [...Array(9).keys()].map((j) =>
+      [...Array(9).keys()].map(
+        (i) =>
+          board.filter((cell) => cell !== 0).map((cell) => cell % 9)[j * 9 + i]
+      )
+    );
+    //row check
+    const rowCheck = (row) => [...new Set(row)].length == 9;
+    const solvedRows = rows.map(rowCheck);
+    console.log(solvedRows);
+    setSolved(rows.filter((row, i) => solvedRows[i]).flat());
+    //set solved based on flat index
+
+    console.log(rows);
+    // const columns = [...Array(9).keys()].map((i) => board);
+
+    // console.log(s);
+    // setSolved(s);
+  }, [board]);
 
   return (
     <>
