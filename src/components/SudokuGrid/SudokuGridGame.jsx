1fb47e2dcba2925232539d4081f8f2f702b9636e
Nicholas Lee <nicholasleexyz@gmail.com>
2023-10-04 20:24:32 -0600

    progress?

--- src/components/SudokuGrid/SudokuGridGame.jsx
+++ src/components/SudokuGrid/SudokuGridGame.jsx
@@ -7,20 +7,35 @@ export default function SudokuGridGame() {
   const { board, setBoard } = useBoardContext();
 
   useEffect(() => {
+    setBoard([...Array(81).keys()].map((i) => i % 9));
+  }, []);
+
+  useEffect(() => {
     const rows = [...Array(9).keys()].map((j) =>
-      [...Array(9).keys()].map(
-        (i) =>
-          board.filter((cell) => cell !== 0).map((cell) => cell % 9)[j * 9 + i]
-      )
+      [...Array(9).keys()].map((i) => board[j * 9 + i])
     );
+    // console.log(rows);
     //row check
     const rowCheck = (row) => [...new Set(row)].length == 9;
     const solvedRows = rows.map(rowCheck);
-    console.log(solvedRows);
-    setSolved(rows.filter((row, i) => solvedRows[i]).flat());
+    // console.log(solvedRows);
+
+    // const s = rows.filter((r, i) => solvedRows[i])
+    const zip = rows.map((row, i) => [row, solvedRows[i]]);
+    // console.log(zip);
+
+    const s = zip
+      .filter((pair) => pair[1])
+      .map((pair) => pair[0])
+      .flat();
+
+    // const s = rows.filter((row, i) => solvedRows[i]).flat();
+    console.log(s);
+    setSolved(s);
+
     //set solved based on flat index
 
-    console.log(rows);
+    // console.log(rows);
     // const columns = [...Array(9).keys()].map((i) => board);
 
     // console.log(s);
